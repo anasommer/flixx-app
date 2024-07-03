4e78eae144a49d58eda23b52fc34681aad4136f1
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-07-03 16:52:43 +0200

    tv shows

--- js/script.js
+++ js/script.js
@@ -34,6 +34,40 @@ async function displayPopularMovies() {
   });
 }
 
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = ` 
+            <a href="tv-details.html?id=${show.id}">
+    ${
+      show.poster_path
+        ? ` <img
+                src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                class="card-img-top"
+                alt="${show.name}"
+              />`
+        : ` <img
+                src="images/no-image.jpg"
+                class="card-img-top"
+                alt="${show.name}"
+              />`
+    }
+            </a>
+            <div class="card-body">
+              <h5 class="card-title">${show.name}</h5>
+              <p class="card-text">
+                <small class="text-muted">Air Date: ${
+                  show.first_air_date
+                }</small>
+              </p>
+            </div>
+          `;
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
 async function fetchAPIData(endpoint) {
   const APIKEY = `be964afe29bba2d4e625f31ce04fedf9`;
   const APIURL = `https://api.themoviedb.org/3/`;
@@ -75,7 +109,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
