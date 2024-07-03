6e54b0599532cec0e375fd2e7325b278983c54e2
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-07-03 16:41:03 +0200

    display popular movies

--- js/script.js
+++ js/script.js
@@ -2,6 +2,51 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = ` 
+          <a href="movie-details.html?id=${movie.id}">
+  ${
+    movie.poster_path
+      ? ` <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+      : ` <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+  }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        `;
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+async function fetchAPIData(endpoint) {
+  const APIKEY = `be964afe29bba2d4e625f31ce04fedf9`;
+  const APIURL = `https://api.themoviedb.org/3/`;
+
+  const response = await fetch(
+    `${APIURL}${endpoint}?api_key=${APIKEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
   links.forEach((link) => {
@@ -15,7 +60,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
